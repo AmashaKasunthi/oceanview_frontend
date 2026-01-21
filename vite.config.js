24f7cc202ba1f9f82c0796dcacf07e14bf4a68a3
AmashaKasunthi <akasunthi@gmail.com>
2026-01-21 11:13:10 +0530

    add proxy

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,17 @@ export default defineConfig({
   plugins: [react()],
 })
 */
+/*import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+
+export default defineConfig({
+  plugins: [
+    react(),
+    tailwindcss(), // ✅ REQUIRED
+  ],
+});
+*/
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
@@ -15,4 +26,13 @@ export default defineConfig({
     react(),
     tailwindcss(), // ✅ REQUIRED
   ],
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:8080",
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
 });
